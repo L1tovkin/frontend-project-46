865ead313da9b92d8a4734634807e93491c6a892
L1tovkin <l1tovkin20@gmail.com>
2025-01-21 22:55:42 +0300

    step 1 and 2

---
+++ bin/gendiff.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+import genDiff from '../index.js';
+
+const program = new Command();
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .action((filepath1, filepath2, options) => {
+    console.log(genDiff(filepath1, filepath2, options.format));
+  });
+
+program.parse(process.argv);
